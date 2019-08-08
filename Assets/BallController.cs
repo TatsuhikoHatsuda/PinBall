19c1329ee3d23723f341f3b3bebfe4d072a4f4b5
TatsuhikoHatsuda <hattu8200@gmail.com>
2019-08-08 16:17:52 +0900

    課題作成
    スコア表示機能を作成

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -11,18 +11,51 @@ public class BallController : MonoBehaviour {
     // ゲームオーバーを表示するテキスト
     private GameObject gameoverText;
 
+    // ゲームの得点
+    private int score = 0;
+    // ゲームの得点を表示するテキスト
+    private GameObject scoreText;
+
 	// Use this for initialization
 	void Start () {
 		// シーン中のGameOverTextオブジェクトを取得
         this.gameoverText = GameObject.Find("GameOverText");
+        // シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+
+        // ScoreTextに得点を表示
+        this.scoreText.GetComponent<Text>().text = "SCORE：" + this.score.ToString("D5");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// ボールが画面外に出た場合
         if (this.transform.position.z < this.visiblePosZ) {
-            // gmaeoverTextにゲームオーバーを表示
+            // GameOverTextにゲームオーバーを表示
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
 	}
+
+    // 衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other) {
+        // 衝突したオブジェクトによって得点を追加
+        if (other.gameObject.tag == "SmallStarTag") {
+            // SmallStarは10点追加
+            this.score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag") {
+            // LargeStarは20点追加
+            this.score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag") {
+            // SmallCloudは30点追加
+            this.score += 30;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag") {
+            // LargeCloudは50点追加
+            this.score += 50;
+        }
+        // ScoreTextに得点を表示
+        this.scoreText.GetComponent<Text>().text = "SCORE：" + this.score.ToString("D5");
+    }
 }
